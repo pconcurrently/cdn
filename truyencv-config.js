d04e722478618beb507e3aa0071dfe21dc44d8c8
Pho Huynh <shortgiraffe4@users.noreply.github.com>
2018-10-01 09:07:44 +0700

    Add to allLT list

--- truyencv-config.js
+++ truyencv-config.js
@@ -1,6 +1,14 @@
 var allLT = {
   ttt: "32",
-  ltt: "7906"
+  ltt: "7906",
+  ntc: "25",
+  hlt: "24",
+  tlq: "26",
+  tnt: "33",
+  ukt: "23",
+  htt: "103",
+  att: "63",
+  hnt: "65",
 };
 
 var selectedLT = "ltt";
